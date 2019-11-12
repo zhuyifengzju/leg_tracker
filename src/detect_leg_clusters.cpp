0e58e92f4c03819dce07a840711edf4f0c77730b
Yifeng Zhu <yifeng.zhu@utexas.edu>
2019-11-12 13:21:19 -0600

    Modification for cobot

--- src/detect_leg_clusters.cpp
+++ src/detect_leg_clusters.cpp
@@ -75,9 +75,9 @@ public:
     nh_.param("scan_topic", scan_topic, std::string("scan"));
     nh_.param("fixed_frame", fixed_frame_, std::string("odom"));
     nh_.param("detection_threshold", detection_threshold_, -1.0);
-    nh_.param("cluster_dist_euclid", cluster_dist_euclid_, 0.13);
+    nh_.param("cluster_dist_euclid", cluster_dist_euclid_, 0.08);
     nh_.param("min_points_per_cluster", min_points_per_cluster_, 3);                
-    nh_.param("max_detect_distance", max_detect_distance_, 10.0);   
+    nh_.param("max_detect_distance", max_detect_distance_, 30.0);   
     nh_.param("marker_display_lifetime", marker_display_lifetime_, 0.2);   
     nh_.param("use_scan_header_stamp_for_tfs", use_scan_header_stamp_for_tfs_, false);
     nh_.param("max_detected_clusters", max_detected_clusters_, -1);
